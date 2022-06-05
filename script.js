bdedfb104a35b213fdef9d9f72645a4a2387be9f
fabriciovg <fabricio.v.gomes47@gmail.com>
2022-06-04 23:16:57 -0300

    atualização 04/06

---
+++ script.js
@@ -0,0 +1,25 @@
+var dataatual = new Date();
+var data;
+var idade = 18;
+var limite = 100;
+
+
+
+    if (data > dataatual)
+    console.log('Essa data não é válida. Tente novamente.');
+
+    else
+        console.log('Data incluída no cadastro!');
+
+
+    if (idade => 18)
+        console.log('Idade Válida.')
+
+    else
+        console.log('Idade não é válida');      
+
+    if (limite > 100)
+        console.log('O limite de participantes é 100. Tente novamente');
+
+    else
+        console.log('Cadastro concluído com sucesso!');        
